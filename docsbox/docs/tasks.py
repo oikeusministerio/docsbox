dda3a19dbeba7194e58722f7e95d910366e70d37
nunomsfranco <nuno.franco@cgi.com>
2018-11-15 18:08:03 +0000

    changes to reflect more the testing env - updated process_document_conversion, changed the use of the ghostscript for presentation type files. (to be discussed)

--- docsbox/docs/tasks.py
+++ docsbox/docs/tasks.py
@@ -59,29 +59,34 @@ def process_convertion(path, options, meta):
 
 
 def process_document_convertion(path, options, meta, current_task):
+    output_path = os.path.join(app.config["MEDIA_PATH"], current_task.id)
     with Office(app.config["LIBREOFFICE_PATH"]) as office:  # acquire libreoffice lock
         with office.documentLoad(path) as original_document:  # open original document
             with TemporaryDirectory() as tmp_dir:  # create temp dir where output'll be stored
                 if options["format"] in app.config[app.config["CONVERTABLE_MIMETYPES"][meta["mimetype"]]["formats"]]:
-                    tmp_path=os.path.join(tmp_dir, current_task.id)
-                    original_document.saveAs(tmp_path, fmt=options["format"])
-                    if options["format"] == "pdf":
-                        file_name = "{0}.{1}".format(meta["filename"], options["format"])
-                        output_path = os.path.join(app.config["MEDIA_PATH"], current_task.id)
+                    file_name = "{0}.{1}".format(meta["filename"], options["format"])
+                    
+                    if app.config["CONVERTABLE_MIMETYPES"][meta["mimetype"]]["formats"] == "PRESENTATION_EXPORT_FORMATS" and options["format"] == "pdf":
+                        tmp_path=os.path.join(tmp_dir, current_task.id)
+                        original_document.saveAs(tmp_path, fmt=options["format"])
                         try:
                             subprocess.check_output(app.config["GHOSTSCRIPT"] + ['-sOutputFile=' + output_path, tmp_path])
                         except:
                             current_task.cancel()
                             return
+                    else:
+                        original_document.saveAs(output_path, fmt=options["format"])
+
+                    # We checks the config for the mimetype of the converted format, expect if its pdf
+                    # Because in the config pdf format is as application/pdfa to diferenciate versions
+                    if options["format"] == "pdf":
                         fileType="application/pdf"
                     else:
-                        # We checks the config for the mimetype of the converted format expect if its pdf
-                        # Because in the config pdf format is as application/pdfa to diferenciate versions
-                        fileType= (value for value in app.config["ACCEPTED_MIMETYPES"] if value["format"] == options["format"])
+                        fileType = (key for key, value in app.config["ACCEPTED_MIMETYPES"].items() if value["format"] == options["format"])
+                        
                 if app.config["THUMBNAILS_GENERATE"] and options.get("thumbnails", None): # generate thumbnails
-                        output_path, file_name = thumbnail_generator(path, options, meta, current_task, original_document, tmp_dir)  
-        file_remove_task = remove_file.schedule(datetime.timedelta(
-            seconds=app.config["RESULT_FILE_TTL"]), output_path)
+                        output_path, file_name = thumbnail_generator(path, options, meta, current_task, original_document, tmp_dir) 
+        file_remove_task = remove_file.schedule(datetime.timedelta(seconds=app.config["RESULT_FILE_TTL"]), output_path)
         current_task.meta["tmp_file_remove_task"] = file_remove_task.id
         current_task.save_meta()
     return {"fileName": file_name, "fileType": fileType }
