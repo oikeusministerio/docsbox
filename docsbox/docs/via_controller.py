cc0949b4ec530be1e94268a5d835a87cc1eb8744
nunomsfranco <nuno.franco@cgi.com>
2019-01-15 18:47:56 +0000

    small changes and error catching

--- docsbox/docs/via_controller.py
+++ docsbox/docs/via_controller.py
@@ -7,8 +7,8 @@ def get_file_from_via(file_id):
     cert=app.config["VIA_CERT_PATH"]
     return get(url=url, cert=cert, stream=True)
 
-def save_file_on_via(file_path, file_type, via_allowed_users):
+def save_file_on_via(file_path, mime_type, via_allowed_users):
     data = open(file_path, "rb")
     cert=app.config["VIA_CERT_PATH"]
-    headers = {'VIA_ALLOWED_USERS': via_allowed_users, 'Content-type': file_type}
+    headers = {'VIA_ALLOWED_USERS': via_allowed_users, 'Content-type': mime_type}
     return post(url=app.config["VIA_URL"], cert=cert, data=data, headers=headers)
