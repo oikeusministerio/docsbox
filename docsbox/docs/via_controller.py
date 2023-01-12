8c490d719e5c63a2119a04b09f0e0fc0756f37a4
Miro Metsänheimo <miro.metsanheimo@cgi.com>
2023-01-12 13:39:26 +0200

    SSY-49 improvements
    * Use threads in gunicorn to enable concurrency in flask requests
    * Fix error handling for when an error happens in the VIA upload request
      the logs would only display "Unserializable return value"
    * Add a retry functionality for VIA requests, retry a maximum of 5 times
      on a 1 second interval on errors

--- docsbox/docs/via_controller.py
+++ docsbox/docs/via_controller.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from requests import get, post
 from docsbox import app
 
@@ -5,11 +8,25 @@ from docsbox import app
 def get_file_from_via(file_id):
     url = app.config["VIA_URL"] + "/" + file_id
     cert = app.config["VIA_CERT_PATH"]
-    return get(url=url, cert=cert, stream=True, timeout=60)
+    return retry(lambda: get(url=url, cert=cert, stream=True, timeout=60))
 
 def save_file_on_via(file_path, mime_type, via_allowed_users):
     with open(file_path, "rb") as data:
         cert = app.config["VIA_CERT_PATH"]
         headers = { 'VIA_ALLOWED_USERS': via_allowed_users, 'Content-type': mime_type }
-        response = post(url=app.config["VIA_URL"], cert=cert, data=data, headers=headers, timeout=60)
+        response = retry(lambda: post(url=app.config["VIA_URL"], cert=cert, data=data, headers=headers, stream=True, timeout=60))
     return response
+
+def retry(call, max = int(app.config["VIA_RETRY_MAX"]), count = 0):
+    try:
+        response = call()
+        if count > 1:
+            logging.warn('Retry successful on count ' + str(count))
+        return response
+    except Exception as e:
+        if count >= max:
+            logging.warn('Maximum retry count exceeded, rethrowing...')
+            raise e
+        logging.warn('Error in VIA call, retrying... : ' + str(e))
+        time.sleep(1)
+        return retry(call, max, count + 1)
