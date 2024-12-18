7a147aad0714f406d64ea38721fc245e3d9fb3a3
filipe.pinto <filipe.pinto@cgi.com>
2024-12-18 16:32:24 +0000

    SSY-5998 Added types

--- docsbox/cleaner/__init__.py
+++ docsbox/cleaner/__init__.py
@@ -1,6 +1,7 @@
 import os
 import json
 from docsbox import app, db
+from docsbox.docs.tasks import FileInfo
 from docsbox.docs.utils import check_file_path
 from datetime import datetime, timedelta
 
@@ -13,20 +14,20 @@ def cleaning_job():
 
     keys_to_remove=[]
     for key in keys :
-        file_info = json.loads(db.get(key))
-        if datetime.strptime(file_info["datetime"], '%Y/%m/%d-%H:%M:%S') < file_ttl:
-            if "file_path" in file_info and check_file_path(file_info["file_path"]):
-                os.remove(file_info["file_path"])
+        file_info: FileInfo = json.loads(db.get(key))
+        if datetime.strptime(file_info.datetime, '%Y/%m/%d-%H:%M:%S') < file_ttl:
+            if file_info.file_path and check_file_path(file_info.file_path):
+                os.remove(file_info.file_path)
             keys_to_remove.append(key)
 
     if keys_to_remove:
         db.delete(*keys_to_remove)
     print("Removed %d entries." % keys_to_remove.__len__())
 
-    if db.keys('fileId:*') == []:
+    if not db.keys('fileId:*'):
         media_file_list = os.listdir(app.config["MEDIA_PATH"])
         print("%d files in media directory for deletion" % media_file_list.__len__())
-        if media_file_list != []:
+        if media_file_list:
             for file in media_file_list:
                 file_path = os.path.join(app.config["MEDIA_PATH"], file)
                 try:
